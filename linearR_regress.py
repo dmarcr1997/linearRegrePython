172f281bcc0d9141156c3c892164f584060dfd72
Damon <dmarcr1997@gmail.com>
2020-07-06 15:29:16 -0500

    created regression from data set

--- linearR_regress.py
+++ linearR_regress.py
@@ -9,3 +9,10 @@ y_axis = dataset.iloc[:, 1]  #data of second col
 
 from sklearn.model_selection import train_test_split
 X_data, X_test, Y_data, Y_test = train_rest_split(x_axis, y_axis, test_size=1/3, random_state=0)
+
+#X_data contians 1st col
+#Y_data contains 2nd col
+
+from sklearn.linear_model import LinearRegresion
+regressor = LinearRegression()
+regressor.fit(X_data, Y_data)
